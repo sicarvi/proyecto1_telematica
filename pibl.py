50c44433faa7cea458f2f5a51b0c262cc4bf747e
Simon Cardenas <scardenasv@eafit.edu.co>
2022-10-13 12:59:20 -0500

    archivo config

--- pibl.py
+++ pibl.py
@@ -32,13 +32,38 @@ class ConexionCliente(threading.Thread):
 HOST = "127.0.0.1"
 PORT = 8080
 
-if __name__ == "__main__":
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
-        servidor.bind((HOST, PORT))
-        print("Servidor listo y esperando")
+def setup(file):
+    params = {}
+    print(file)
+    try:
+        with open(file) as conf:
+            for line in conf:
+                if line.startswith("PUERTO"):
+                    p = line[line.index("=")+1:]
+                    params["puerto"] = p
+                elif line.startswith("HOSTS"):
+                    p = line[line.index("=")+1:]
+                    p = p.split(",")
+                    print(p)
+                    params["hosts"] = p
+        return True, params
+    except:
+        print("falsed")
+        return False, params
 
-        while True:
-            servidor.listen()
-            socketcliente, dircliente = servidor.accept()
-            thread_cliente = ConexionCliente(dircliente, socketcliente)
-            thread_cliente.start()
+if __name__ == "__main__":
+
+    check, params = setup("serv.config")
+
+    if check:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
+            servidor.bind((HOST, PORT))
+            print("Servidor listo y esperando")
+
+            while True:
+                servidor.listen()
+                socketcliente, dircliente = servidor.accept()
+                thread_cliente = ConexionCliente(dircliente, socketcliente)
+                thread_cliente.start()
+    else:
+        print("Por favor ingrese un archivo de configuracion en el directorio establecido.")
