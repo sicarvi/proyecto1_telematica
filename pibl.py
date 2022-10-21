7b08394becfdfb1703f41e3828472418302b15d7
Simon <scardenasv@eafit.edu.co>
2022-10-21 14:55:59 -0500

    implementacion inicial proxy

--- pibl.py
+++ pibl.py
@@ -1,4 +1,4 @@
-import socket, os , time, sys
+import socket, os , time#TODO, sys
 import threading
 class ConexionCliente(threading.Thread):
     body = ""
@@ -14,32 +14,43 @@ class ConexionCliente(threading.Thread):
         self.log(f"[NUEVA CONEXION]: {ip_cliente}")
         #self.log(f"Ip del cliente: {ip_cliente[0]}")
         
-
+    def app_connection(self, request, puerto, ip):
+        response = ""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as aplicacion:
+            aplicacion.bind((puerto,ip))
+            aplicacion.listen()
+            socket_app, dir_ip = aplicacion.accept()
+            with socket_app:
+                print(f"Connected by {dir_ip}")
+                socket_app.sendall(request)
+                data = socket_app.recv(4096)
+                response = data.decode()
+                    
+        return response
     
     def run(self):
         self.log(f"[NUEVO THREAD]: {self.name}")
         request = ''
         self.socket_cliente.sendall(bytes("Servidor dice hola",'UTF-8'))
         while True:
-            data = self.socket_cliente.recv(2048)
+            data = self.socket_cliente.recv(4096)
             request = data.decode()
+            self.log(f"* REQUEST {self.ip_cliente}* {request}")
             if request=='exit':
               break
-            self.socket_cliente.send(bytes(request,'UTF-8'))#echo para permitir envio continuo
-            self.log(f"* REQUEST {self.ip_cliente}* {request}")
+            ip, puerto = round_robin()
+            response = self.app_connection(request,ip,puerto)
+            self.socket_cliente.sendall(bytes(response,'UTF-8'))
+            self.log(f"* RESPONSE {self.ip_cliente}* {response}")
 
         self.log(f"[FINALIZO CONEXION]: {self.ip_cliente}")
         return
 
-    def crearHttp(ip, body): 
-        #necesito request, ip_cliente[0]
+    
 
-        pass
-
-
-
-HOST = "127.0.0.1"
-PORT = 8080
+def crear_http_request(ip, body): 
+    #necesito request, ip_cliente[0]
+    pass
 
 def setup(file):
     params = {}
@@ -65,7 +76,7 @@ if __name__ == "__main__":
 
     if check:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
-            servidor.bind((HOST, PORT))
+            servidor.bind((params["puerto"], params["hosts"]))
             print("Servidor listo y esperando")
             while True:
                 servidor.listen()
